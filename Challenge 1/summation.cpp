7985d51e049172b102dc807bba4ba43c42a62678
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-03-30 18:51:52 +0530

    Update summation.cpp
    Updated summation.cpp to use the MyTimer.h header file. The compilation command remains the same. We will use GCC 7.2 to run this code.

--- Challenge 1/summation.cpp
+++ Challenge 1/summation.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
-#include <chrono>
-using namespace std;
+#include <MyTimer.h>
 int main()
 {
 	using ull=unsigned long long;
-	ull n, sum=0; cin>>n;
-	auto start=chrono::high_resolution_clock::now();
-	#pragma omp parallel for reduction(+:sum)
+	ull n, sum=0; std::cin>>n;
+	auto start=now();
+    #pragma omp parallel for reduction(+:sum)
 	for(int i=1; i<=n; i++)
 		sum+=i;
-	auto end=chrono::high_resolution_clock::now();
-	cout<<sum<<"\n";
-	chrono::duration<double> diff=end-start;
-	cout<<diff.count()<<"\n";
+    auto end=now();
+    //std::cout<<sum<<"\n";
+    std::cout<<span(start, end)<<"\n";
 }
