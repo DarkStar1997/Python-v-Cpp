310dda87066e3efcd6c39fdddd01a4645bc64f45
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-03-30 18:53:40 +0530

    Fixed indentation

--- Challenge 1/summation.cpp
+++ Challenge 1/summation.cpp
@@ -5,10 +5,10 @@ int main()
 	using ull=unsigned long long;
 	ull n, sum=0; std::cin>>n;
 	auto start=now();
-    #pragma omp parallel for reduction(+:sum)
+	#pragma omp parallel for reduction(+:sum)
 	for(int i=1; i<=n; i++)
 		sum+=i;
-    auto end=now();
-    //std::cout<<sum<<"\n";
-    std::cout<<span(start, end)<<"\n";
+	auto end=now();
+	//std::cout<<sum<<"\n";
+	std::cout<<span(start, end)<<"\n";
 }
