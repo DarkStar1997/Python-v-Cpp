617b8eb7de14020b71d386c289ed5cfaa974b3c0
jit89 <chatterjee.soumyajit19@gmail.com>
2018-04-03 02:28:48 +0530

    Added string_concat_join file for benchmarking

---
+++ Challenge 2/string_concat_join.py
@@ -0,0 +1,21 @@
+import time
+import array
+
+'''
+Strings are immutable in Python, so one of the most popular and efficient means of creating a single large string
+out of smaller things is by using the join method. Here, map converts each integer recieved from xrange to its
+equivalent string representation. The list of strings from 1 to N returned by map is then converted to a large string
+using the join method.
+'''
+
+if __name__ == '__main__':
+
+    n = int(input())
+    
+    start = time.time()
+
+    ''.join(map(str,xrange(1, n+1)))
+    
+    end = time.time()
+    
+    print end - start
