622a0a3be7f702d3112c9cb77d3b4980f1788fd2
jit89 <chatterjee.soumyajit19@gmail.com>
2018-04-02 18:43:40 +0530

    Added hashbang to summation_for.py

--- Challenge 1/summation_for.py
+++ Challenge 1/summation_for.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python
 import time
+
 if __name__ == '__main__':
     n = int(input())
-    start = time.time()
     Sum = 0
+    start = time.time()
     for i in xrange(1, n+1):
         Sum += i
     end = time.time()
