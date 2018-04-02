ad9c7cb6a3e9ec877616b7183365cbc3d70a9b68
Soumyojit Chatterjee <37982803+jit89@users.noreply.github.com>
2018-04-03 02:07:59 +0530

    Updated summation_for.py docstring

--- Challenge 1/summation_for.py
+++ Challenge 1/summation_for.py
@@ -1,7 +1,12 @@
 #!/usr/bin/env python
 
 '''
-The basic for loop in python is slow, python is dynamically typed so the type of a variable cannot be fixed beforehand. Here, the variable i goes from 1 to n, where n would be in the order of 10^8, so the type of the variable i has to be checked on every iterations and the allocated space be resized when required. xrange is used in place of range here (python2.7 only), range would have been faster but xrange being a generator, is more efficient. The order of the input provided for benchmarking will be 10^8, so a call to range(10**8) would slow things down considerably due to the fact that the interpreter will try to allocate a huge amount of memory to contain integers from 1 to 10^8. 
+The basic for loop in python is slow, python is dynamically typed so the type of a variable cannot be fixed beforehand. 
+Here, the variable i goes from 1 to n, where n would be in the order of 10^8, so the type of the variable i has to be checked 
+on every iterations and the allocated space be resized when required. xrange is used in place of range here (python2.7 only),
+range would have been faster but xrange being a generator, is more efficient. The order of the input provided
+for benchmarking will be 10^8, so a call to range(10**8) would slow things down considerably due to the fact
+that the interpreter will try to allocate a huge amount of memory to contain integers from 1 to 10^8. 
 '''
 
 import time
