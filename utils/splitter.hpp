bc6483a5455fd385f331f58f184b0536390d5be6
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-06-11 21:38:19 +0530

    Added switching support between fread and ifstream

--- utils/splitter.hpp
+++ utils/splitter.hpp
@@ -1,4 +1,3 @@
-#pragma once
 #include <string>
 #include <vector>
 #include <fstream>
@@ -26,45 +25,80 @@ std::vector<std::string> split(const std::string &text, std::unordered_set<char>
 }
 std::vector<std::string> file_tokenize(const std::string &filename, std::unordered_set<char> delimiters, const size_t buf_size)
 {
-    FILE *in=fopen(filename.c_str(), "r");
-    std::vector<char> buffer(buf_size);
-    std::vector<std::string> sentences;
-    std::string str_tmp="";
-    size_t sent_size=0;
-    bool ctrl=false;
-    while(size_t x=std::fread(buffer.data(), 1, buf_size, in))
-    {
-        //std::cout<<"Currently read:\n";
-        //std::cout<<std::string(buffer.begin(), buffer.begin()+x)<<"\n****\n";
-        if(x==0)
-            break;
-        auto tmp=split(std::string(buffer.begin(), buffer.begin()+x), delimiters);
-        //std::cout<<"tmp_size="<<tmp.size()<<"\n";
-        if(ctrl)
+    #ifdef SPLITTER_USE_IFSTREAM
+        std::ifstream in; in.open(filename);
+        std::vector<char> buffer(buf_size);
+        buffer.reserve(buf_size);
+        std::vector<std::string> sentences;
+        std::string str_tmp="";
+        size_t sent_size=0;
+        bool ctrl=false;
+        in.read(buffer.data(), buf_size);
+        while(size_t x=in.gcount())
         {
-            //std::cout<<"ctrl=true\n";
-            auto tmp_size=tmp.size();
-            sentences[sent_size-1]+=tmp[0];
-            for(size_t i=1; i<tmp_size; i++)
+            if(x==0)
+                break;
+            auto tmp=split(std::string(buffer.begin(), buffer.begin()+x), delimiters);
+            if(ctrl)
             {
-                sentences.emplace_back(tmp[i]);
-                sent_size++;
+                auto tmp_size=tmp.size();
+                sentences[sent_size-1]+=tmp[0];
+                for(size_t i=1; i<tmp_size; i++)
+                {
+                    sentences.emplace_back(tmp[i]);
+                    sent_size++;
+                }
             }
+            else
+            {
+                auto tmp_size=tmp.size();
+                for(auto& i:tmp)
+                {
+                    sentences.emplace_back(i);
+                    sent_size++;
+                }
+                str_tmp=tmp[tmp_size-1];
+            }
+            ctrl=(delimiters.find(buffer[x-1])==delimiters.end());
+            in.read(buffer.data(), buf_size);
         }
-        else
+        in.close();
+    #else
+        FILE *in=fopen(filename.c_str(), "r");
+        std::vector<char> buffer(buf_size);
+        std::vector<std::string> sentences;
+        std::string str_tmp="";
+        size_t sent_size=0;
+        bool ctrl=false;
+        while(size_t x=std::fread(buffer.data(), 1, buf_size, in))
         {
-            //std::cout<<"ctrl=false\n";
-            auto tmp_size=tmp.size();
-            for(auto& i:tmp)
+            if(x==0)
+                break;
+            auto tmp=split(std::string(buffer.begin(), buffer.begin()+x), delimiters);
+            if(ctrl)
             {
-                sentences.emplace_back(i);
-                sent_size++;
+                auto tmp_size=tmp.size();
+                sentences[sent_size-1]+=tmp[0];
+                for(size_t i=1; i<tmp_size; i++)
+                {
+                    sentences.emplace_back(tmp[i]);
+                    sent_size++;
+                }
             }
-            str_tmp=tmp[tmp_size-1];
+            else
+            {
+                auto tmp_size=tmp.size();
+                for(auto& i:tmp)
+                {
+                    sentences.emplace_back(i);
+                    sent_size++;
+                }
+                str_tmp=tmp[tmp_size-1];
+            }
+            ctrl=(delimiters.find(buffer[x-1])==delimiters.end());
         }
-        ctrl=(delimiters.find(buffer[x-1])==delimiters.end());
-    }
-    fclose(in);
+        fclose(in);
+    #endif
     return sentences;
 }
 std::vector<std::string> file_tokenize(const std::string &filename, std::unordered_set<char> delimiters)
