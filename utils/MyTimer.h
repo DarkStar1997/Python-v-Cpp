02103adec7d8b662ad72ce59d88a79ebc63a6a32
Rohan Mark Gomes <rohanmarkgomes@gmail.com>
2018-10-06 20:10:27 +0530

    Update MyTimer.h
    Changed from high_resolution_clock to steady_clock for improved accuracy

--- utils/MyTimer.h
+++ utils/MyTimer.h
@@ -1,9 +1,9 @@
 #include <chrono>
-std::chrono::high_resolution_clock::time_point now()
+std::chrono::steady_clock::time_point now()
 {
-	return std::chrono::high_resolution_clock::now();
+	return std::chrono::steady_clock::now();
 }
-double span(std::chrono::high_resolution_clock::time_point t1, std::chrono::high_resolution_clock::time_point t2)
+double span(std::chrono::steady_clock::time_point t1, std::chrono::steady_clock::time_point t2)
 {
 	std::chrono::duration<double> diff=t2-t1;
 	return diff.count();
